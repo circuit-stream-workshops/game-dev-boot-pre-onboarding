49354f0133926f40351e979f10bf0562e9c558d6
Indika Wijesooriya <indikman@gmail.com>
2023-11-06 11:45:53 -0700

    Base project without assets

---
+++ Assets/Project/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace CircuitStream { 
+    public class GameManager : MonoBehaviour
+    {
+        //super basic singleton
+        public static GameManager Instance;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+        //----------------------
+
+        [SerializeField] private TMP_Text scoreText;
+        [SerializeField] private AudioSource sfxAudio;
+        [SerializeField] private AudioClip pickupAudioClip;
+
+        int totalTreasures;
+        int collectedTreasures;
+
+        void Start()
+        {
+            totalTreasures = GameObject.FindGameObjectsWithTag("treasure").Length;
+            collectedTreasures = 0;
+            scoreText.text = collectedTreasures + "/" + totalTreasures;
+        }
+
+        public void CollectTreasure()
+        {
+            //play audio
+            sfxAudio.PlayOneShot(pickupAudioClip);
+
+            collectedTreasures++;
+
+            if(collectedTreasures>=totalTreasures)
+            {
+                GameWin();
+            }
+
+            scoreText.text = collectedTreasures + "/" + totalTreasures;
+        }
+
+        public void GameOver()
+        {
+            Debug.Log("Game Over");
+        }
+
+        public void GameWin()
+        {
+            Debug.Log("You Win!");
+        }
+    }
+}
---
+++ Assets/Project/Scripts/PlayerLogic.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CircuitStream
+{
+    public class PlayerLogic : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("treasure"))
+            {
+                //collect treasure
+                Destroy(other.gameObject);
+                GameManager.Instance.CollectTreasure();
+            }
+            else if (other.CompareTag("ghost"))
+            {
+                //game over
+                GameManager.Instance.GameOver();
+            }
+        }
+    }
+}
